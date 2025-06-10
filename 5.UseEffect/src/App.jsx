c5b4f788b8ce9ce8a24a9ff043d7b69b7ac674c7
elif_ <elifery06@yandex.com>
2025-06-10 15:44:51 +0300

    useEffect kullanımı

---
+++ 5.UseEffect/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  useEffect(() => {
+    console.log("her zmaan çalışır")
+  })
+  useEffect(() => {
+    console.log("ilk render edildiğinde çalışır")
+    //veritabanından ürünleri getir ve listele
+  }, [])
+
+  useEffect(() => {
+    console.log("ilk render edildiğinde FİRSTNAME ve LASTNAME state  değerleri değiştiğinde çalışır")
+  }, [firstName, lastName])
+  // useEffect(() => {
+  //   console.log("ilk render edildiğinde ve LASTNAME state değeri değiştiğinde çalışır")
+  // }, [lastName])
+  return (
+    <div>
+      <div><button onClick={() => setFirstName("Emre")}>Adı değiştir</button></div>
+      <div><button onClick={() => setLastName("x")}>Soyismi değiştir</button></div>
+    </div>
+  )
+}
+
+export default App
