3d4047694eb1616271237a0130269e43dcf8182c
elif_ <elifery06@yandex.com>
2025-06-17 18:45:35 +0300

    update

--- 8.api-using/src/App.jsx
+++ 8.api-using/src/App.jsx
@@ -20,16 +20,23 @@ function App() {
     const response = await axios.post(`${BASE_URL}/users`, newUser)
     console.log(response.data)
   }
-
+  const updateUser = async (userId, updatedUser) => {
+    await axios.put(`${BASE_URL}/users/${userId}`, updatedUser);
+  }
 
   useEffect(() => {
     // getAllUsers()
     // getUserById(1)
-    const newUser = {
-      "username": "elif",
-      "password": "763"
-    }
-    createUser(newUser)
+    // const newUser = {
+    //   "username": "elif",
+    //   "password": "763"
+    // }
+    // createUser(newUser)
+
+    // updateUser("688d", {
+    //   "username": "yoti",
+    //   "password": "5656"
+    // })
   }, [])
   return (
     <div>
