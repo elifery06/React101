6659bee6a9cc19477e2566ca31c7375e62723b23
elif_ <elifery06@yandex.com>
2025-06-23 17:18:50 +0300

    to do app

---
+++ 10.TODO-App/src/components/TodoCreate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoCreate() {
+    return (
+        <div className='todo-create'>
+            <input type="text" placeholder='Todo giriniz' />
+            <button>Todo oluştur</button>
+        </div>
+    )
+}
+
+export default TodoCreate
